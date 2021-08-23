d2003022053a771f95ae395d841a6925d84a205b
bbedotor <53658555+bbedotor@users.noreply.github.com>
2021-08-23 16:43:07 -0500

    unit test preparation

---
+++ Todobrayan.Test/Helpers/TestFactory.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Todobrayan.Common.Models;
+using Todobrayan.Functions.Entities;
+
+namespace Todobrayan.Test.Helpers
+{
+    public class TestFactory
+    {
+        public static Todoentity GetTodoentity()
+        {
+            return new Todoentity
+            {
+                ETag = "*",
+                PartitionKey = "TODO",
+                RowKey = Guid.NewGuid().ToString(),
+                CreatedTime = DateTime.UtcNow,
+                IsCompleted = false,
+                TaskDescription = "Task: delete all"
+            };
+        }
+
+        public static DefaultHttpRequest CreateHttpRequest(Guid todoId, Todo todoRequest)
+        {
+            string request = JsonConvert.SerializeObject(todoRequest);
+            return new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Body = GenerateStreamFromString(request),
+                Path = $"/{todoId}"
+            };
+        }
+
+        public static DefaultHttpRequest CreateHttpRequest(Guid todoId)
+        {
+
+            return new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Path = $"/{todoId}"
+            };
+        }
+
+        public static DefaultHttpRequest CreateHttpRequest(Todo todoRequest)
+        {
+            string request = JsonConvert.SerializeObject(todoRequest);
+            return new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Body = GenerateStreamFromString(request)
+            };
+        }
+
+        public static DefaultHttpRequest CreateHttpRequest()
+        {
+
+            return new DefaultHttpRequest(new DefaultHttpContext());
+        }
+
+        public static Todo getTodoRequest()
+        {
+            return new Todo
+            {
+                CreatedTime = DateTime.UtcNow,
+                IsCompleted = false,
+                TaskDescription = "Try to delete all."
+            };
+        }
+
+
+        private static Stream GenerateStreamFromString(string strinToConvert)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(strinToConvert);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+
+        public static ILogger CreateLogger (LoggerTypes type = LoggerTypes.Null)
+        {
+            ILogger logger;
+
+            if (type == LoggerTypes.List)
+            {
+                logger = new ListLogger();
+            }
+            else
+            {
+                logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+            }
+
+            return logger;
+        } 
+    }
+}
